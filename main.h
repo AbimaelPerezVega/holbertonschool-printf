fac7182dca5459a8bf0664f3af65f6d23bbc4bf8
jm <jjmendezrodriguez@yahoo.com>
2024-03-25 10:55:55 -0400

    create main.h

---
+++ main.h
@@ -0,0 +1,39 @@
+#ifndef PRINTF_CUSTOM_H
+#define PRINTF_CUSTOM_H
+
+#include <stdio.h>
+#include <stdarg.h>
+/**
+ * _print - function for printf
+ *
+ */
+void _print(const char *format, ...)
+{
+	va_list args;
+	va_start(args, format);
+
+	while (*format != '\0')
+	{
+		if (*format == '%')
+		{
+			format++;
+			if (*format == 'd')
+			{
+				int num = va_arg(args, int);
+				printf("%d", num);
+			}
+			else if (*format == 's')
+			{
+				char *str = va_arg(args, char *);
+				printf("%s", str);
+			}
+		}
+		else
+		{
+			putchar(*format);
+		}
+		format++;
+	}
+	va_end(args);
+}
+#endif /*PRINTF_CUSTOM_H */
