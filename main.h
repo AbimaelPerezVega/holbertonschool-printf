c28ef1b7aaa2db9588d57c2f7bb27ae2444de390
Abimael <icemask2612@gmail.com>
2024-03-28 19:14:55 -0400

    testing

--- main.h
+++ main.h
@@ -1,50 +1,71 @@
 #ifndef MAIN_H
 #define MAIN_H
+#include <stdarg.h>
+#include <stdio.h>
+#include <unistd.h>
+
+#define UNUSed(x) (void)(x)
+#define BUFF_SIZE 1024
+
+/* FLAGS */
+#define F_MINUS 1
+#define F_PLUS 2
+#define F_ZERO 4
+#define F_HASH 8
+#define F_SPACE 16
+/* SIZE */
+#define S_LONG 2
+#define S_SHORT 1
+/**
+ *struct fmt - struct up
+ *L0MAX
+ *@fmt: the format
+ *@fn: the function associated
+ *
+*/
+struct fmt
+{
+char fmt;
+int (*fn)(va_list, char[], int, int, int, int);
+};
+/**
+ *typedef struct fmt fmt_t - struct op
+ *
+ *@fmt: the format
+ *@fm_t: the function associated
+ *
+*/
+typedef struct fmt fmt_t;
 
 int _printf(const char *format, ...);
+int handle_print(const char *fmt, int *i,
+va_list list, char buffer[], int flags, int width, int precision, int size);
 
+/***FUNCTIONS***/
+/*Functions to print chars and strings*/
+int print_char(va_list types, char buffer[],
+		int flags, int width, int precision, int size);
+int print_string(va_list types, char buffer[],
+		int flags, int width, int precision, int size);
+int print_percent(va_list types, char buffer[],
+		int flags, int width, int precision, int size);
+/*Functions to print numbers*/
+int print_int(va_list types, char buffer[],
+int flags, int width, int precision, int size);
+int print_binary(va_list types, char buffer[],
+int flags, int width, int precision, int size);
+int print_unsigned(va_list types, char buffer[],
+int flags, int width, int precision, int size);
+int print_octal(va_list types, char buffer[],
+int flags, int width, int precision, int size);
+int print_hexadecimal(va_list types, char buffer[],
+int flags, int width, int precision, int size);
+int print_hexa_upper(va_list types, char buffer[],
+int flags, int width, int precision, int size);
+int print_hexa(va_list types, char buffer[],
+int flags, int width, int precision, int size);
+
+/*Function to print non printable characters */
+int print_non_printable(va_list types, char buffer[],
+int flags, int width, int precision, int size);
 #endif /* MAIN_H */
-
-#ifndef PRINTF_CUSTOM_H
-#define PRINTF_CUSTOM_H
-
-#include <stdio.h>
-#include <stdarg.h>
-/**
- * _printf - function for printf
- * @format: the thing
- *
- * Return: 0 the number of characters printed
- */
-int _printf(const char *format, ...)
-{
-	va_list args;
-va_start(args, format);
-
-		while (*format != '\0')
-	{
-		if (*format == '%')
-		{
-			format++;
-			if (*format == 'd')
-			{
-				int num = va_arg(args, int);
-printf("%d", num);
-			}
-			else if (*format == 's')
-			{
-				char *str = va_arg(args, char *);
-printf("%s", str);
-			}
-		}
-		else
-		{
-			putchar(*format);
-		}
-		format++;
-	}
-	va_end(args);
-
-	return (0);
-}
-#endif /*PRINTF_CUSTOM_H */
